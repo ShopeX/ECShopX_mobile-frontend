29042e0aeae3a0f00c58b67203a3b7b86cb404aa
bentz <lv.bentz@gmail.com>
2019-02-27 20:15:42 +0800

    no message

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -11,7 +11,7 @@ export default class Index extends Component {
   componentWillMount () {
     if (process.env.NODE_ENV === 'development') {
       Taro.redirectTo({
-        url: '/pages/member/index'
+        url: '/pages/cart/index'
       })
     }
   }
