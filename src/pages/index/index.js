6a5df0d4b3a6d1ddffac6c8b57bec46a00baf336
chenhui0212 <chenhui@shopex.cn>
2019-03-13 18:56:01 +0800

    二维码

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -9,8 +9,8 @@ export default class Index extends Component {
 
   componentWillMount () {
     Taro.redirectTo({
-      url: '/pages/member/point-draw-detail?id=268'
-      // url: '/pages/auth/login'
+      url: '/pages/member/recommend'
+      // url: '/pages/auth/reg'
       // url: '/pages/item/espier-detail?id=268'
     })
   }
