c8bc3014b7063b01610f4f1017a394c1fc11abac
bentz <lv.bentz@gmail.com>
2019-03-22 10:43:05 +0800

    no message

--- src/api/req.js
+++ src/api/req.js
@@ -101,7 +101,7 @@ class API {
     // }
     options.data = {
       ...(options.data || {}),
-      company_id: 2
+      company_id: APP_COMPANY_ID
     }
     if (options.method === 'GET') {
       options.url = addQuery(options.url, qs.stringify(options.data))
