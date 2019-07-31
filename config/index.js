f77e2150ed90cd6d449eaec1cf2c3b1a4bdbb441
tangyongchuan <tangyongchuan@shopex.cn>
2019-07-31 15:51:30 +0800

    提交h5编译

--- config/index.js
+++ config/index.js
@@ -45,11 +45,11 @@ const config = {
     APP_COMPANY_ID: '1',
     APP_INTEGRATION: process.env.INTEGRATION_APP,
 
-    APP_HOME_PAGE: '/pages/index',
-    // APP_AUTH_PAGE: '/pages/auth/login',
-    APP_AUTH_PAGE: TARO_ENV === 'weapp'
-      ? '/pages/auth/wxauth'
-      : '/pages/auth/login',
+    APP_HOME_PAGE: '"/pages/index"',
+    APP_AUTH_PAGE: '"/pages/auth/login"',
+    // APP_AUTH_PAGE: TARO_ENV === 'weapp'
+    //   ? '/pages/auth/wxauth'
+    //   : '/pages/auth/login',
 
     APP_GIFT_APPID: 'wx2fb97cb696f68d22' // 礼品小程序 appid
   },
