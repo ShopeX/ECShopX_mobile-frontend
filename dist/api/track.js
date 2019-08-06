010060cac4ad0d07f4615f9ff2d7cef3f61de269
tangyongchuan <tangyongchuan@shopex.cn>
2019-08-06 17:09:10 +0800

    提交更新

--- dist/api/track.js
+++ dist/api/track.js
@@ -1,18 +1,1 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.viewnum = viewnum;
-
-var _req = require("./req.js");
-
-var _req2 = _interopRequireDefault(_req);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function viewnum() {
-  var param = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
-  return _req2.default.post('/track/viewnum', {});
-}
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.viewnum=viewnum;var _req=require("./req.js"),_req2=_interopRequireDefault(_req);function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}function viewnum(){0<arguments.length&&void 0!==arguments[0]&&arguments[0];return _req2.default.post("/track/viewnum",{})}
