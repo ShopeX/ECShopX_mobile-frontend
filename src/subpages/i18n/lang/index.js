a9541436d208b213e3b2ccece1b8b91c18ab8f55
zhangjing1 <zhangjing@shopex.cn>
2026-01-30 15:00:18 +0800

    翻译

--- src/subpages/i18n/lang/index.js
+++ src/subpages/i18n/lang/index.js
@@ -103,16 +103,17 @@ const withStorageCommonLang =
 // 从本地存储中获取通用语言，如果不存在则使用空字符串
 const commonLang = withStorageCommonLang
   ? globalThis.localStorage.getItem('')
-  : process.env.APP_DEFAULT_LANGUAGE
+  : ''
 // 从本地存储中获取当前语言，如果不存在则使用源语言
 const baseLang = withStorageLang
   ? globalThis.localStorage.getItem('lang')
   : process.env.APP_DEFAULT_LANGUAGE
 console.log('baseLang', baseLang, process.env.APP_DEFAULT_LANGUAGE)
 console.log('commonLang', commonLang)
-const lang = commonLang ? commonLang : baseLang
+const lang = baseLang || commonLang || process.env.APP_DEFAULT_LANGUAGE || 'en'
 // 根据当前语言设置翻译函数的语言包
 globalThis.$t.locale(globalThis.langMap[lang], 'lang')
 globalThis.$changeLang = (lang) => {
+  console.log('globalThis.$changeLang', lang)
   globalThis.$t.locale(globalThis.langMap[lang], 'lang')
 }
